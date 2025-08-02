d964575cfbbb0fa5a31cd9d38364c755f27df9d2
nurislam243 <mdnurislam15177@gmail.com>
2025-08-02 16:08:50 +0600

    first commit

---
+++ output/script.js
@@ -0,0 +1,4 @@
+"use strict";
+console.log("hello");
+const country = "bangladesh Usa India";
+console.log(country);
---
+++ src/script.ts
@@ -0,0 +1,3 @@
+console.log("hello");
+const country = "bangladesh Usa India";
+console.log(country);
